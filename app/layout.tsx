f5aa44c9ff39195b442ca141a8aadc40acab58af
walenso <asimoya23@gmail.com>
2024-12-31 13:10:07 -0800

    Layout of the main app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,17 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import '@/assets/styles/globals.css';
-import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from '@/lib/constants';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/assets/styles/globals.css";
+import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from "@/lib/constants";
 
-
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    template: `%s | ${APP_NAME}`, 
-  default:APP_NAME
-},
+    template: `%s | ${APP_NAME}`,
+    default: APP_NAME,
+  },
   description: APP_DESCRIPTION,
-  metadataBase: new URL(SERVER_URL)
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
@@ -21,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className}`}>{children}</body>
     </html>
   );
